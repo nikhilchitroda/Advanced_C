4570c522e2661af0df83023ef79b4303fb5320ff
nvchitroda <nvchitroda@gmail.com>
2022-06-10 11:14:01 +0530

    Added: Created the example based on advaced flow.

---
+++ advancedFlow/main.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<setjmp.h>
+
+jmp_buf buf;
+
+void error_recovery() 
+{
+	printf("detected an undrecoverable error\n");
+	longjmp(buf, 1);
+}
+
+int main()  
+{
+
+	while (1) 
+	{
+		if (setjmp(buf)) 
+		{
+			printf("back in main\n");
+			break;
+		}
+		else 
+		{
+			error_recovery();
+		}
+	}
+	return 0;
+}
